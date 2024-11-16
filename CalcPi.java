a5c8c9dbb20cd8d7609d2e98cef7c090d98c1154
lianlevwe <lianlevwe@gmail.com>
2024-11-16 11:49:56 +0200

    fix CalcPi

--- CalcPi.java
+++ CalcPi.java
@@ -3,16 +3,13 @@ public class CalcPi {
 	public static void main(String [] args) { 
 		int num = Integer.parseInt(args[0]);
 		double pi = 0;
-		double denominator = 1;
-
+		double denominator = 1.0;
 		for (int i = 0; i < num; i++) {
-			if (i % 2 == 0) pi += (1 / denominator);
-			else pi -= (1 / denominator);
-			denominator += 2;
+			pi = (i % 2 == 0) ? pi + (1.0 / denominator) : pi - (1.0 / denominator);
+			denominator += 2.0;
 		}
 	    System.out.println("pi, according to Java: " + Math.PI);
-		// System.out.println("pi, approximated:     " + (pi * 4));
-		System.out.println("pi, approximated:     3.140592653839794");
+		System.out.println("pi, approximated:     " + (pi * 4));
 
 	}
 }
